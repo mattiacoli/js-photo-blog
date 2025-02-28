2ffd0a23271d79bb6a0b4019c04fa92805749885
mattiacoli <mattia.coli@aol.com>
2025-02-28 11:58:21 +0100

    add photo from Api by Ajax call

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,35 @@
-console.log('hi');
+// get element node
 
 const photoBlogUrlEndpoint = 'https://lanciweb.github.io/demo/api/pictures/'
+const containerRowEl = document.querySelector('.container .row')
+
+fetch(photoBlogUrlEndpoint)
+  .then(res => res.json())
+  .then(photos => {
+
+    console.log(photos);
+
+    photos.forEach(photo => {
+      console.log(photo);
+
+      const markup = `
+        <div class="col-lg-4">
+          <figure>
+            <div class="pin">
+              <img src="./assets/img/pin.svg" alt="">
+            </div>
+            <img src=${photo.url} alt=''  class="img-fluid"/>
+            <figcaption>
+              <div class="date">${photo.date}</div>
+              <div class="title">${photo.title}</div>
+            </figcaption>
+          </figure>          
+        </div>
+      `
+
+      containerRowEl.insertAdjacentHTML('beforeend', markup)
+
+
+    });
+
+  })
